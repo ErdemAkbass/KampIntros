35e221da67dd0a5d37fe09c6066041bfcdf48cbd
Selim Somar <selimsomar@gmail.com>
2022-12-02 14:19:25 +0300

    Add project files.

---
+++ example/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            menu menu1 = new menu();
+            menu1.Adı = "öğrenci menü";
+            menu1.içindekiler = "pizza,soğan halkası,nugget,1L kola";
+            menu1.fiyat=90;
+            menu1.ıd = 1;
+            
+
+            menu menu2 = new menu();
+            menu2.Adı = "çılgın menü";
+            menu2.içindekiler = "döner,patates,nugget,kutu icetea";
+            menu2.fiyat = 100;
+            menu2.ıd = 2;
+
+            menu menu3 = new menu();
+            menu3.Adı = "special menü";
+            menu3.içindekiler = "hamburger,patates,soğan halkası,kutu kola";
+            menu3.fiyat = 110;
+            menu3.ıd = 3;
+
+            menu menu4 = new menu();
+            menu4.Adı = "aile menü";
+            menu4.içindekiler = "büyük boy pizza,patates,soğan halkası,nugget,1lt içecek";
+            menu4.fiyat = 180;
+            menu4.ıd = 4;
+
+            menu[] menuler = new menu[] { menu1, menu2, menu3, menu4 };
+
+            for (int i = 0; i < menuler.Length; i++)
+            {
+                Console.WriteLine("Ürün Adı:"+menuler[i].Adı);
+                Console.WriteLine("Ürünün içindekileri:" + menuler[i].içindekiler);
+                Console.WriteLine("Ürün Fiyatı:" + menuler[i].fiyat);
+                Console.WriteLine("Ürün ID:" + menuler[i].ıd+"\n");
+            }
+            foreach (menu menu in menuler)
+            {
+                Console.WriteLine("Ürün Adı:" + menu.Adı + "\n" + "Ürün İçindekileri" + menu.içindekiler + "\n"+"Ürün Fiyatı:"+menu.fiyat+"\n"+"Ürün Id"+menu.ıd+"\n");
+            }
+        }
+        class menu
+        {
+            public string Adı;
+            public string içindekiler;
+            public int fiyat;
+            public int ıd;
+        }
+    }
+}
---
+++ eğitim02/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace eğitim02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+       
+    
+        product urun1 = new product();
+        urun1.urunadı = "sweatshirt";
+                urun1.urunfiyat = 200;
+                urun1.urunrenk = "lacivert";
+                urun1.urunıd = 287634;
+
+                product urun2 = new product();
+        urun2.urunadı = "ayakkabı";
+                urun2.urunfiyat = 450;
+                urun2.urunrenk = "siyah";
+                urun2.urunıd = 376289;
+
+                product urun3 = new product();
+        urun3.urunadı = "telefon";
+                urun3.urunfiyat = 15000;
+                urun3.urunrenk = "siyah";
+                urun3.urunıd = 938476;
+
+                product urun4 = new product();
+        urun4.urunadı = "saat";
+                urun4.urunfiyat = 1000;
+                urun4.urunrenk = "beyaz";
+                urun4.urunıd = 917982;
+
+                product[] urunler = new product[] { urun1, urun2, urun3, urun4 };
+
+                for (int i = 0; i<urunler.Length; i++)
+                {
+                    Console.WriteLine("Ürun adı:" + urunler[i].urunadı);
+                    Console.WriteLine("Ürün Fiyat:" + urunler[i].urunfiyat);
+                    Console.WriteLine("Ürün Renk:" + urunler[i].urunrenk);
+                    Console.WriteLine("Ürün Id:" + urunler[i].urunıd+"\n");
+                }
+            foreach (var product in urunler)
+            {
+                Console.WriteLine("Ürün Adı:" + urun1.urunadı + "Ürün Fiyatı:" + urun2.urunfiyat + "Ürün Rengi:" + urun3.urunrenk + "Ürün Id" + urun4.urunıd + "\n");
+            }
+}
+
+class product
+{
+    public string urunadı;
+    public int urunfiyat;
+    public string urunrenk;
+    public int urunıd;
+}
+    }
+}
