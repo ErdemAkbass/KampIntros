139f2d93bb2edf44b505c0cb819547b83bc46b93
Selim Somar <selimsomar@gmail.com>
2022-12-16 02:29:28 +0300

    generic

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+     internal class MyDictionary<TKey,TValue> // generic class
+    {
+        TKey[] _key;
+        TValue[] _Value;
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _Value = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKeyArray = _key;
+            TValue[] tempValueArray = _Value;
+            _key = new TKey[_key.Length + 1];
+            _Value = new TValue[_Value.Length + 1];
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                _key[i] = tempKeyArray[i];
+            }
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                _Value[i] = tempValueArray[i];
+            }
+            _key[_key.Length - 1] = key;
+            _Value[_Value.Length - 1] = value;
+
+
+        }
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+            
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(06, "Ankara");
+            dictionary.Add(34, "İstanbul");
+            dictionary.Add(35, "İzmir");
+            dictionary.Add(69, "Bayburt");
+           
+
+            Console.WriteLine(dictionary.Count);
+        }
+    }
+}
